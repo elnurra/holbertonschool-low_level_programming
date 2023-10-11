d5013f5eea63f516f0773f50dad6ab1f2650b076
Elnur <elnurra@code.edu.az>
2023-10-11 06:50:31 -0700

    feat: so bad environmoment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
+/*
+ *    File: 4-puts.c
+ *    Elnur Azizov
+ */
 #include <stdio.h>
 /*
  * main - Prints "Programming is like building a multilingual
  *                  puzzle, followed by a new line.
- *   
- * Return: Always 0.
+ *                 Return: Always 0.
  */
 int main(void)
 {
