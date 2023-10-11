86208cb5a6eeba2bd000a2206d803210849a4fbb
Elnur <elnurra@code.edu.az>
2023-10-11 12:18:38 -0700

    feat: added 2 advanced tasks

---
+++ hello_world/9-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: Always 0.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
