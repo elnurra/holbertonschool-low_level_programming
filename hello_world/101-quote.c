662c95f9c2d5a07425c87c7bfb81a0b50e604442
Elnur <elnurra@code.edu.az>
2023-10-11 12:32:55 -0700

    feat: added task

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that print a line using the printf function
- * Return: Always 0.
+ * Return: Always 1 (Success).
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
