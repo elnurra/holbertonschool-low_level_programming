2f85b488ff46ff1bac5e4c67af5244ca69feb2da
Elnur <elnurra@code.edu.az>
2023-10-11 11:55:40 -0700

    fixed task

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function 
- * Return: Always 0.
+* main - A program that print a line using the printf function
+* Return: Always 0.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
