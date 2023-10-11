d2ea769bc023ec93029ea96a7fff0507422da58f
Elnur <elnurra@code.edu.az>
2023-10-11 06:23:32 -0700

    feat: added comment for 4 task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 
 
 #include <stdio.h>
-
+/**
+ *  * main function - print for console "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: means returns Always 0 for function.
+*      */
 
 int main(void)
 {
