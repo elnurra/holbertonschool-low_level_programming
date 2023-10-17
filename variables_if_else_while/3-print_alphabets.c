63d2a937dba9acf851022f3234eb133d653c162b
Elnur <elnurra@code.edu.az>
2023-10-17 04:26:42 -0700

    bug-fix: fixed some bugs

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                putchar(letter);
-        for (letter = 'A'; letter <= 'Z'; letter++)
-                putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 	putchar('\n');
 
-        return (0);
+	return (0);
 }
