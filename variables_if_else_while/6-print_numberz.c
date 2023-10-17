0d3d1ff84106414c51418f966d5d3456ff92820a
Elnur <elnurra@code.edu.az>
2023-10-17 04:51:47 -0700

    feat: added 6-print_numberz.c file

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the digits with chars.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+	putchar('\n');
+	return (0);
+}
