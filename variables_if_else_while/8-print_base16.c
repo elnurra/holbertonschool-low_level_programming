e7489f82825172c1107582ca01787f58109b19ca
Elnur <elnurra@code.edu.az>
2023-10-17 06:29:12 -0700

    feat: added hexadecimal task

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the digits in.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
