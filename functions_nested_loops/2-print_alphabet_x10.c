0bcd3a4cec682d426090cf6bfde463510e8acf5e
Elnur <elnurra@code.edu.az>
2023-10-18 15:07:10 -0700

    feat: added 2 task

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that print alphabet
+ *
+ * Return: no return
+*/
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	
+		_putchar('\n');
+	}
+}
