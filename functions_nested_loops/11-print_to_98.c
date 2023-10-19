352ad48e431aea94dbcfb677f7232d770eb4b5f2
Elnur <elnurra@code.edu.az>
2023-10-19 15:19:06 -0700

    feat: added task

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural
+ * numbers from n to 98
+ * @n:integer to start by
+ *return: returns nothing
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				printf("\n");
+				break;
+			}
+			printf("%d, ", i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				printf("\n");
+				break;
+			}
+			printf("%d, ", i);
+		}
+	}
+	_putchar('\n');
+}
