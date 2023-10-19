1338c29cf0c0e337b8fcef91e79079bb09f681f5
Elnur <elnurra@code.edu.az>
2023-10-19 14:41:22 -0700

    feat: added 10 task

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - sums up two numbers
+ *@num1: first integer
+ *@num2: second integer
+ *Return: returns 0
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
