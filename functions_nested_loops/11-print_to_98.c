70901b4721a73137932b9db398dae9d1ffedf1b6
Elnur <elnurra@code.edu.az>
2023-10-19 15:24:11 -0700

    bug-fix: gixed count of stmolobs

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,6 @@ void print_to_98(int n)
 			if (i == 98)
 			{
 				printf("%d", i);
-				printf("\n");
 				break;
 			}
 			printf("%d, ", i);
@@ -30,7 +29,6 @@ void print_to_98(int n)
 			if (i == 98)
 			{
 				printf("%d", i);
-				printf("\n");
 				break;
 			}
 			printf("%d, ", i);
