d9fca5108eeba3b6741c354d3c9db1f03c7ced63
Elnur <elnurra@code.edu.az>
2023-10-22 02:28:23 -0700

    feat: added task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal
+ * @n: Amount of \ to be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if(n <= 0)
+	{
+		_putchar('\n');
+       	}
+	else
+		for (i = 0; n > 0; n--, i++)
+		{
+			for(j = 0; j < i ; j++ )
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+}
