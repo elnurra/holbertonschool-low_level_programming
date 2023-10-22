2cdcc7c1712eba8b99f931b4c80e9da95bafb151
Elnur <elnurra@code.edu.az>
2023-10-22 01:31:00 -0700

    bux-fix: fixed betty documantion for task 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,9 +2,9 @@
 
 /**
  * mul - Multiply 2 variables
- * @a - first variable
- * @b - second variable
- * Return: result of 2 multipliying variables
+ * @a: first operand
+ * @b: second operand
+ * Return: product of 2 operands
  */
 
 int mul(int a, int b)
