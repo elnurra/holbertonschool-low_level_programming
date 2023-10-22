7d1d07414c936de3fe90186ba036f796a266b38b
Elnur <elnurra@code.edu.az>
2023-10-22 07:45:45 -0700

    bug-fix: fixed task

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Computes and prints the sum of multiples of 3 or 5 below 1024.
@@ -7,7 +6,7 @@
  * Return: Always 0.
  */
 
-int print_to_sum(void)
+int main(void)
 {
 	int sum = 0;
 	int number;
