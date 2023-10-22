b91ec94aacfcdfe2ce4e90d2412206b7d2437365
Elnur <elnurra@code.edu.az>
2023-10-22 01:28:57 -0700

    feat: added task 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 variables
+ * @a - first variable
+ * @b - second variable
+ * Return: result of 2 multipliying variables
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
