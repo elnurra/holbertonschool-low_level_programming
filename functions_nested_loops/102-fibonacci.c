a2fad798185166cf1382e147d4b76818c8b8f1ba
Elnur <elnurra@code.edu.az>
2023-10-22 08:08:26 -0700

    bug-fix: fixed variables

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -8,19 +8,20 @@
 int main(void)
 {
 	int n = 50;
-	long long F0 = 1;
-	long long F1 = 1;
-	long long Fn;
+	long F0 = 1;
+	long F1 = 1;
+	long Fn;
+	int i;
 
-	printf("%lld, %lld", F0, F1);
+	printf("%ld, %ld", F0, F1);
 
-	for (int i = 2; i < n; i++)
+	for (i = 2; i < n; i++)
 	{
 		Fn = F0 + F1;
 		F0 = F1;
 		F1 = Fn;
 
-		printf(", %lld", Fn);
+		printf(", %ld", Fn);
 	}
 
 	printf("\n");
