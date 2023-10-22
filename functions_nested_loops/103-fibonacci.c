1b9fd1776b6c18d4e768f7f97ac03970a44ec96f
Elnur <elnurra@code.edu.az>
2023-10-22 08:25:20 -0700

    feat: added 13 task

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 33;
+	long F0 = 1;
+	long F1 = 2;
+	long Fn;
+	int i;
+	long result;
+
+	printf("%ld, %ld", F0, F1);
+
+	printf("\n");
+	for (i = 2; i < n; i++)
+	{
+		Fn = F0 + F1;
+		F0 = F1;
+		F1 = Fn;
+		if (Fn <= 4000000)
+		{
+			if (Fn % 2 == 0)
+			{
+				result += Fn;
+				printf("%ld, ", result);
+				printf("\n");
+			}
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
