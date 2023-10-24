2cb5334926b9a6eb1cf69855fb93e90bd187287b
Elnur <elnurra@code.edu.az>
2023-10-24 10:30:38 -0700

    feat: added 1-5 task

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(s[length] != '\0')
+		length++;
+
+	return (length);
+}
