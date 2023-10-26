78d1d8258296bef7cc6715da679fd698b20f24ad
Elnur <elnurra@code.edu.az>
2023-10-26 06:22:03 -0700

    feat: added task 0-strcat.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,3 +10,8 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+/**
+ * Pointers Arrays in C
+ */
+char *_strcat(char *dest, char *src);
