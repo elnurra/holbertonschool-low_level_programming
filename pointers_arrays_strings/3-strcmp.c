79d4879de7382f005b70aa8e355fe3524d81fa8b
Elnur <elnurra@code.edu.az>
2023-10-28 13:11:38 -0700

    feat: fixed task 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,12 +2,10 @@
 
 /**
  * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
- * @s1: first string to compare
- * @s2: second string to compare
- *
- * Return: 0 if the strings are equal, a positive value if s1 is greater than
- * s2, and a negative value if s1 is less than s2
+ * Return: difference between the two strings
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,15 +14,9 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-		{
-			return ((s1[i] > s2[i]) ? 15 : -15);
-		}
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
-	}
-	return ((s1[i] > s2[i]) ? 15 : -15);
+	return (s1[i] - s2[i]);
 }
 
