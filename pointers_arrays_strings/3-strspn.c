6397fdd3b1581cbb8efb9d08228ee393c958b7ba
Elnur <elnurra@code.edu.az>
2023-10-29 05:15:48 -0700

    feat: added task 2-5

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets length of prefix substr
+ * @s: String being evaluated
+ * @accept: Bytes being compared
+ *
+ * Return: Number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i]; ++i)
+	{
+		for (j = 0; accept[j]; ++j)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (i);
+}
---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String being searched
+ * @needle: Substring being searched for
+ *
+ * Return: Pointer to beginning of located substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i]; ++i)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; haystack[i] == needle[j]; ++i, ++j)
+				;
+			if (needle[j] == '\0')
+				return (haystack + i - j);
+		}
+	}
+	return (0);
+}
