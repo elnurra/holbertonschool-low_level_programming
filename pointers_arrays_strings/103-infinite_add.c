09e6ce49af3543e51a51a68aa046ee9ebacc9736
Elnur <elnurra@code.edu.az>
2023-10-29 07:37:32 -0700

    feat: added all tasks

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,67 +1,46 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * infinite_add - Add up two numbers stored in given char arrays
  * @n1: The first number
  * @n2: The second number
  * @r: Pointer to the buffer to store result
  * @size_r: The size of the buffer
- *
  * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int len1 = strlen(n1), len2 = strlen(n2), carry = 0;
+	int start = 0, i = len1 - 1, j = len2 - 1, k = 0, end;
 
-	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || l2 + 2 > size_r)
+	if (len1 + len2 > size_r - 1)
 		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
+	while (i >= 0 || j >= 0 || carry)
 	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
+		int sum = carry;
+
+		if (i >= 0)
 		{
-			carry = 1;
-			sum -= 10;
+			sum += n1[i] - '0';
+			i--;
 		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		rl++;
-	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmpl = 0;
-	while (i <= rl)
-	{
-		tmp[i] = r[rl - i];
-		tmpl++;
-		i++;
-	}
-	i = 0;
-	while (i < tmpl)
-	{
-		if (r[i] == '\0')
+		if (j >= 0)
 		{
-			break;
+			sum += n2[j] - '0';
+			j--;
 		}
-		r[i] = tmp[i];
-		i++;
+		carry = sum / 10;
+		r[k] = (sum % 10) + '0';
+		k++;
+	}
+	r[k] = '\0';
+	for (start = 0, end = k - 1; start < end; start++, end--)
+	{
+		char temp = r[start];
+
+		r[start] = r[end];
+		r[end] = temp;
 	}
 	return (r);
 }
