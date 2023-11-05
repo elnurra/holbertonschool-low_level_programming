8d47f18264d23eb153b2ad6625978a5294131df5
Elnur <elnurra@code.edu.az>
2023-11-05 01:04:36 -0800

    feat: added task 100-is_palindrome.c

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = 0, i;
+
+	while (*(s + len))
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		if (*(s + i) != *(s + len - i - 1))
+			return (0);
+	}
+
+	return (1);
+}
+
