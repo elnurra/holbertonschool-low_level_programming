d2cd494dcc0810d8a4927bea04f73bb0dcb24888
Elnur <elnurra@code.edu.az>
2023-11-05 01:58:34 -0700

    bug-fix: fixed task 100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,72 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
 /**
- * divide_helper - Helper function to perform division
- * @cents: The amount of money to be divided
- *
- * Return: The minimum number of coins needed
- */
-int divide_helper(int cents)
-{
-	int coins = 0;
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
-	}
-
-	return (coins);
-}
-
-/**
- * main - Calculates the minimum number of coins needed to make change
- * @argc: The number of arguments passed to the program
- * @argv: An array of pointers to the arguments
- *
- * Return: 0 on success, 1 on error
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]);
-	int coins = divide_helper(cents);
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", coins);
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
