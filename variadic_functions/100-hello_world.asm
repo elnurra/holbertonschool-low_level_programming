003ff73cf3b5bc7e3d0ca8b3cd2670267993ab9d
Elnurra <elnurra@code.edu.az>
2023-11-25 08:15:24 -0800

    fixing

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,27 +1,19 @@
 section .data
-    hello_msg db 'Hello, World', 0xA ; Message to be printed (0xA is the newline character)
+    hello db 'Hello, World', 0xA
+    hello_len equ $ - hello
 
 section .text
     global _start
 
 _start:
-    ; File descriptor for stdout (1)
-    mov rdi, 1
-    
-    ; Message length
-    mov rdx, 13 ; Length of 'Hello, World' plus the newline character
-    
-    ; Pointer to the message
-    mov rsi, hello_msg
-    
-    ; System call number for write (1)
-    mov rax, 1
-    
-    ; Make system call
-    syscall
-    
-    ; Exit the program (syscall number 60 for exit)
-    mov rax, 60
-    xor rdi, rdi ; Exit code 0
+    ; write hello message
+    mov eax, 1
+    mov edi, 1
+    mov esi, hello
+    mov edx, hello_len
     syscall
 
+    ; exit program
+    mov eax, 60
+    xor edi, edi
+    syscall
---
+++ variadic_functions/main.c
@@ -0,0 +1,4 @@
+// main.c
+int main() {
+    return 0;
+}
