d8b0876cfa7cd4a50f28121464becd15068f655e
Elnurra <elnurra@code.edu.az>
2023-12-13 14:11:15 -0800

    feat: added task 0-read_textfile.c

---
+++ file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef FILE_IO
+#define FILE_IO
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
