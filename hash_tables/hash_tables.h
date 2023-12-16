9303ff45341a12cb537eab93f1d45fc3f10a691d
Elnurra <elnurra@code.edu.az>
2023-12-16 13:55:45 -0800

    feat: added task 6-hash_table_delete.c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -41,4 +41,5 @@ int hash_table_update(hash_table_t *ht, char *key_copy, char *val_copy,
 								unsigned long int index, hash_node_t *new_node);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
