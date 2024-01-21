33f86626cb8e66bda2462cfe48b09c675f6b489f
Elnurra <elnurra@code.edu.az>
2024-01-21 00:38:14 -0800

    feat: added task linear seacr alrithm

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+
+
+
+#endif
